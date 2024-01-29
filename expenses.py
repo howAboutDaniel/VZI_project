64b615d0da609c894e7090481f8e4788706182b2
howAboutDaniel <daniel.dlugos@me.com>
2024-01-29 21:19:21 +0100

    Aktualizovaná štruktúra výdajov

--- expenses.py
+++ expenses.py
@@ -1,8 +1,13 @@
 class Expense:
-    def __init__(self, name, category, amount) -> None:
-        self.name = name
-        self.category = category
-        self.amount = amount
+    def __init__(self, date, currency, name, amount, category, reciever, account, note) -> None:
+        self.date = date               # date of an expense
+        self.currency = currency       # currency of an expense
+        self.name = name               # name of an expense
+        self.amount = amount           # expense's amount
+        self.category = category       # expense's category
+        self.reciever = reciever       # reciever of an expense
+        self.account = account         # 3 letter account shortcut, from which an expense was made
+        self.note = note               # short description of an expense
 
     def __repr__(self):
-        return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f} >"
+        return f"<Expense: {self.date}, {self.currency}, {self.name}, ${self.amount:.2f}, {self.category}, {self.reciever}, {self.account}, {self.note} >"
--- tracker.py
+++ tracker.py
@@ -1,6 +1,5 @@
 def main():
     print(f"Welcome to your Ledger")
-    pass
     # Get user input for expense!
     get_user_input()
 
@@ -9,17 +8,16 @@ def main():
     # Read the file and summ expenses!
     summarize()
 
-def get_user_input():
-    print("Record an expense: ")
-    pass
-
+def get_user_input(): # Funkcia pre vytvorenie uživateľvho výdaju
+    print("Record an expense!")
+    expense_name = input("Enter your expense name")
+    print(f"Expense name entered: {expense_name}")
+    
 def log_expense():
-    print(" ")
-    pass
+    print("Saving your expense")
 
 def summarize():
-    print(" ")
-    pass
+    print("Preview of your expenses: ")
 
 if __name__ == "__main__": # Comment! 9'33"
     main()
