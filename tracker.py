9527b4898b9f2a5eb95c48a55a1ded30c328f375
howAboutDaniel <daniel.dlugos@me.com>
2024-01-29 19:42:40 +0100

    Vytvorenie súboru tracker.py, v ktorom je uložený hlavný script

---
+++ tracker.py
@@ -0,0 +1,25 @@
+def main():
+    print(f"Welcome to your Ledger")
+    pass
+    # Get user input for expense!
+    get_user_input()
+
+    # Write the expense to a file!
+    log_expense()
+    # Read the file and summ expenses!
+    summarize()
+
+def get_user_input():
+    print("Record an expense: ")
+    pass
+
+def log_expense():
+    print(" ")
+    pass
+
+def summarize():
+    print(" ")
+    pass
+
+if __name__ == "__main__": # Comment! 9'33"
+    main()
